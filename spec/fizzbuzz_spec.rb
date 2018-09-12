4589bf8ba411f328c04dcbcd400e96b710c1a5e2
NaomiBid <naomi0bid@gmail.com>
2018-09-12 18:30:39 +0100

    fizzbuzz done

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,30 @@
 require 'fizzbuzz'
-
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'return "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+  it 'return "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq "buzz"
   end
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  it 'return "fizz" when passed other multiples of 3' do
+    expect(fizzbuzz(6)).to eq "fizz"
+    expect(fizzbuzz(9)).to eq "fizz"
+    expect(fizzbuzz(12)).to eq "fizz"
   end
-  it 'returns "number" when passed 7' do
-    expect(fizzbuzz(7)).to eq(7)
+  it 'return "fizzbuzz" when passsed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'return "fizzbuzz" when passed other multiples of 15' do
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+    expect(fizzbuzz(45)).to eq "fizzbuzz"
+  end
+  it 'return number when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
+  it 'return "buzz" when passed other multiples of 5' do
+    expect(fizzbuzz(10)).to eq "buzz"
+    expect(fizzbuzz(25)).to eq "buzz"
+    expect(fizzbuzz(20)).to eq "buzz"
   end
 end
